3e96d58a1cf04116389146568ebf0ae4fdc6b7cf
Rasea <raseamarble@gmail.com>
2025-08-13 22:27:15 +0200

    oops (back)

--- github4.js
+++ github4.js
@@ -37,7 +37,7 @@ const xaeModule = {
         notifier: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/notifier.js", done: true },
         layout: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/layoutoptions.js", done: true },
         userlist: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/userlist.js", done: true },
-        enhancedEmotes: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/immergrok/r@enhancedEmotes/enhancedEmotes.js", done: true },
+        enhancedEmotes: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/immergrok/r@2ccd72bed80a45750c4d1e4273484a8e87ffb92a/enhancedEmotes.js", done: true },
         html2canvas: { active: 1, rank: -1, url: "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js", done: true }
     },
     getScript(url, success, cache = true) {
