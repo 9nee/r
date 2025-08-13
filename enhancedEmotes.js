83d1b8b105c1bb53dd25a35da912243a04083ad5
Rasea <raseamarble@gmail.com>
2025-08-14 01:32:02 +0200

    fug

--- enhancedEmotes.js
+++ enhancedEmotes.js
@@ -1,7 +1,4 @@
-//My ass is not removing the getScript addition by Xae, it's too useful for our purposes
-
-
-//Hijack the backend ui.js export function
+//Hijack the cs-emotes-export backend element
 $('#cs-emotes-export').off('click')
 $("#cs-emotes-export").on('click', () => {
     var emoteObject = CHANNEL.emotes.map( (emote) => {
@@ -21,12 +18,13 @@ soundpostButton.style.backgroundImage = soundpostState
     : "url('https://raw.githubusercontent.com/om3tcw/r/refs/heads/emotes/emotes/medicated.png')";
 soundpostButton.style.backgroundSize = "cover";
 
-soundpostButton.on("click", () => {
+$(soundpostButton).on("click", () => {
     soundpostState = !soundpostState;
     setCookie("soundpostState", soundpostState);
     soundpostButton.style.backgroundImage = soundpostState
         ? "url('https://raw.githubusercontent.com/om3tcw/r/refs/heads/emotes/emotes/schizo.gif')"
         : "url('https://raw.githubusercontent.com/om3tcw/r/refs/heads/emotes/emotes/medicated.png')";
 });
+
 const chatInputRow = document.getElementById("chatinputrow");
 chatInputRow.appendChild(soundpostButton);
