60c13575acd8c28141fdded05279476e85e550e9
Rasea <raseamarble@gmail.com>
2025-08-14 04:23:32 +0200

    stuffs

--- github4.js
+++ github4.js
@@ -37,7 +37,7 @@ const xaeModule = {
         notifier: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/notifier.js", done: true },
         layout: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/layoutoptions.js", done: true },
         userlist: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/userlist.js", done: true },
-        enhancedEmotes: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/immergrok/r@40b57739584f3c67a2bbcb02668f0bb71f305027/enhancedEmotes.js", done: true },
+        enhancedEmotes: { active: 1, rank: -1, url: "https://cdn.jsdelivr.net/gh/immergrok/r@dc5afc1600600f4667230fb08763645062c0e054/enhancedEmotes.js", done: true },
         html2canvas: { active: 1, rank: -1, url: "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js", done: true }
     },
     getScript(url, success, cache = true) {
