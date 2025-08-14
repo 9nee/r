ce66d03214b94e58e1c63da3da3fd46a30859b64
Rasea <raseamarble@gmail.com>
2025-08-14 22:24:28 +0200

    I should probably do this differently, somehow.

--- githubLoader.js
+++ githubLoader.js
@@ -29,7 +29,7 @@ const USERLIST_ENHANCEMENT = "userlist.js";
 const ENHANCED_EMOTES = "enhancedEmotes.js";
 const HOLOPEEK = "holoPeek.js"
 
-const CURRENT_COMMIT = "80e4350cdb7fce519ed2a5293beae29ec2400625"
+const CURRENT_COMMIT = "b76cb09adcddbcb9dd1d9c904d24718c5be59e13"
 const CURRENT_REPO = "immergrok"
 //Change to om3tcw on live
 
