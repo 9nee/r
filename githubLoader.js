70904920ffc5f616c16fad3e45bf412b15f845d8
Rasea <raseamarble@gmail.com>
2025-08-16 01:00:41 +0200

    gwak

--- githubLoader.js
+++ githubLoader.js
@@ -13,7 +13,7 @@ if (!this[CHANNEL.name].favicon) {
 }
 
 //Defining some top level variables.
-const CURRENT_COMMIT = "69cc915ee9569a51353c9148ef814ca3bc841bd9"
+const CURRENT_COMMIT = "6a6761056b5259c9b271cfc2715dfc5a6bbea591"
 //Change to om3tcw on live
 const CURRENT_REPO = "immergrok"
 
