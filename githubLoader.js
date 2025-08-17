915bd2068b18289611c9b47add816d24b21fc7a7
9nee <227023053+9nee@users.noreply.github.com>
2025-08-17 13:19:48 -0300

    Remove unused code (old imagePreview and html2canvas)

--- githubLoader.js
+++ githubLoader.js
@@ -110,7 +110,6 @@ const xaeModule = {
         enhancedEmotes: { active: 1, rank: -1, url: makeLiveCDNLink(ENHANCED_EMOTES), done: true },
         holoPeek: { active: 1, rank: -1, url: makeLiveCDNLink(HOLOPEEK), done: true },
         mahjongMode: { active: 1, rank: -1, url: makeLiveCDNLink(MAHJONG_MODE), done: true },
-        html2canvas: { active: 1, rank: -1, url: "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js", done: true },
         nndChatModule: {active: 0, rank: -1, url: makeLiveCDNLink(NND_MODULE), done: true },
         imgPreviewModule: {active: 0, rank: -1, url: makeLiveCDNLink(IMG_PREVIEW_MODULE), done: true }
 
@@ -315,41 +314,6 @@ $(window).bind('keydown', function (event) {
     }
 });
 
-// Image Hover
-const ImageHoverEnable = false;
-
-//TODO: broken for like 4 years now
-function createHoverImage(jqChatMessage) {
-    jqChatMessage.find("a").bind("mouseenter", function () {
-        if (ImageHoverEnable) {
-            const messageAfter = $(this).parent().next();
-            if (!messageAfter.is("img")) {
-                const newImg = new Image();
-                newImg.style.display = "none";
-                newImg.onload = function () {
-                    this.classList.add("imageHoverPreview", "imageLoaded");
-                };
-                newImg.src = $(this).html();
-                $(this).parent().after(newImg);
-            }
-            $("#messagebuffer div:hover .imageHoverPreview").stop(true, false).slideDown(100);
-            $("#messagebuffer div:hover").one("mouseout", function () {
-                $(this).children(".imageHoverPreview").stop(true, true).slideUp(100).delay(100).removeAttr("style");
-            });
-        }
-    });
-}
-
-$("#messagebuffer").bind('DOMNodeInserted', function (event) {
-    $(event.target).find("a").parent().parent().each(function () {
-        createHoverImage($(this));
-    });
-});
-
-$("#messagebuffer a").parent().parent().each(function () {
-    createHoverImage($(this));
-});
-
 // UI Enhancements
 (() => {
     'use strict';
@@ -465,11 +429,6 @@ $('#messagebuffer').off('click').click(e => {
 });
 
 // Slav's Enhancements
-
-let html2canvasScript = document.createElement('script');
-html2canvasScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js';
-document.head.appendChild(html2canvasScript);
-
 function runescape($message) {
 
     const text = $message.text().replace('/runescape', '');
@@ -500,7 +459,6 @@ function runescape($message) {
 
 
 function yayConfetti($message) {
-    
     const $text = $message.text().replace('/yay', '');
     $message.text($text);
 
