99528c3198d474ca6fda31b857465d3dc0353392
9nee <227023053+9nee@users.noreply.github.com>
2025-08-17 14:01:39 -0300

    I'm going to do it (actually enable the module)

--- githubLoader.js
+++ githubLoader.js
@@ -111,7 +111,7 @@ const xaeModule = {
         holoPeek: { active: 1, rank: -1, url: makeLiveCDNLink(HOLOPEEK), done: true },
         mahjongMode: { active: 1, rank: -1, url: makeLiveCDNLink(MAHJONG_MODE), done: true },
         nndChatModule: {active: 0, rank: -1, url: makeLiveCDNLink(NND_MODULE), done: true },
-        imgPreviewModule: {active: 0, rank: -1, url: makeLiveCDNLink(IMG_PREVIEW_MODULE), done: true }
+        imgPreviewModule: {active: 1, rank: -1, url: makeLiveCDNLink(IMG_PREVIEW_MODULE), done: true }
 
     },
     getScript(url, success) {
