1da2b776c0308864e0134ba7cbe381085cffa2c3
9nee <227023053+9nee@users.noreply.github.com>
2025-08-18 12:19:15 -0300

    Test hover

--- custom_modules/imagePreview.js
+++ custom_modules/imagePreview.js
@@ -23,6 +23,53 @@ function createHoverImage(jqChatMessage) {
     });
 }
 
+function createHoverImage2(jqChatMessage) {
+    jqChatMessage
+    .querySelectorAll("a")
+    .forEach(a =>{
+        a.addEventListener("mouseenter", ({target}) => {
+            var img = target.parentElement.querySelector("img")
+            if (!img) {
+                img = new Image();
+                img.style.position = "fixed";
+                img.onload = function () {
+                    this.classList.add("imageHoverPreview", "imageLoaded");
+                };
+                img.referrerPolicy = "no-referrer";
+
+                img.src = target.href;
+
+                if (target.href.search(/youtube\.com|youtu\.be/i) > -1) {
+                    let videoUrl = (new URL(target.href));
+                    let videoId = videoUrl.searchParams.get("v") || videoUrl.pathname.split("/").at(1);
+
+                    img.src = videoId ? `https://i.ytimg.com/vi/${videoId}/mqdefault.jpg` : img.src;
+                }
+
+
+                target.parentElement.appendChild(img);
+            }
+            img.style.display = "";
+        });
+
+        a.addEventListener("mouseout", ({target}) =>{
+            target.parentElement.querySelector("img").style.display = "none";
+        });
+
+        a.addEventListener("mousemove", ({target, clientX, clientY}) =>{
+            let offset = 30;
+            let img = target.parentElement.querySelector("img");
+            if (img.clientWidth + clientX + offset > window.innerWidth) {
+                clientX -= (img.clientWidth + offset * 2.0);
+            }
+            if (img.clientHeight + clientY + offset > window.innerHeight) {
+                clientY -= img.clientHeight + offset * 2.0;
+            }
+            img.style.top  = `${clientY + offset}px`;
+            img.style.left = `${clientX + offset}px`;
+        });
+    });
+}
 
 function toggleImagePreview() {
     let cookie = readCookie("ImagrePreview");
@@ -33,7 +80,7 @@ function toggleImagePreview() {
                 mutationList.filter(m => m.type === 'childList').forEach(m => {
                     Array.from(m.addedNodes)
                         .filter(n => n.nodeType == Node.ELEMENT_NODE)
-                        .forEach(node => createHoverImage($(node)));
+                        .forEach(node => createHoverImage2(node));
                 })
             );
 
@@ -46,7 +93,7 @@ function toggleImagePreview() {
 
         // a -> span -> div
         $("#messagebuffer a").parent().parent().each(function () {
-            createHoverImage($(this));
+            createHoverImage2($(this)[0]);
         });
     } else {
         if (window.imagePreview) {
