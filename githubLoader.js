efc275922c6c3f0c9f466752ae527c52d795c901
Rasea <raseamarble@gmail.com>
2025-08-19 18:10:07 +0200

    wehh

--- githubLoader.js
+++ githubLoader.js
@@ -12,7 +12,7 @@ if (![CHANNEL.name].favicon) {
         .appendTo("head");
 }
 
-const CURRENT_COMMIT = "28ac759e91dc243b6cfd9cac3de014df19fc083b"
+const CURRENT_COMMIT = "f799b65f102090b81f6e349634b8ee23c7fd9b90"
 
 //Change to om3tcw when live
 const CURRENT_BRANCH = "immergrok"
