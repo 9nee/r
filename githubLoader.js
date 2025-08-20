0a88316d896515b6310cd9e215a01122154b8998
Rasea <raseamarble@gmail.com>
2025-08-20 02:19:33 +0200

    `penos

--- githubLoader.js
+++ githubLoader.js
@@ -12,7 +12,7 @@ if (![CHANNEL.name].favicon) {
         .appendTo("head");
 }
 
-const CURRENT_COMMIT = "dd162a5c56950891aaba45a6fc68eb87884dfedf"
+const CURRENT_COMMIT = "39aa14b06010589df02effbc02e736c669629d00"
 
 //Change to om3tcw when live
 const CURRENT_BRANCH = "immergrok"
