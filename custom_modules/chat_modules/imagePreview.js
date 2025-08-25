b60d1886d5f100254badea71697fc983fd99b861
9nee <227023053+9nee@users.noreply.github.com>
2025-08-25 16:17:37 -0300

    Test

--- custom_modules/chat_modules/imagePreview.js
+++ custom_modules/chat_modules/imagePreview.js
@@ -14,12 +14,16 @@ function createHoverImage($linkElement,
     newImg.style.display = "block";
     newImg.referrerPolicy = "no-referrer";
     newImg.src = $linkElement.attr("href");
+    newImg.onload = function () {
+      $(this).slideUp(200);
+    };
+
     $linkElement.data('imageInstance', newImg)
     $(newImg).css({
       'position': 'absolute',
       'z-index': '9999',
       'display': 'block',
-      'top': event.pageY - yOffset,
+      'top': event.pageY + yOffset,
       'left': event.pageX + xOffset
     });
     $('body').append(newImg);
@@ -38,7 +42,7 @@ function createHoverImage($linkElement,
   $linkElement.on("mouseleave", () => {
     const imageElement = $linkElement.data('imageInstance');
     if (imageElement) {
-      $(imageElement).slideUp(200, () => {
+      $(imageElement).slideDown(200, () => {
         $linkElement.removeData('imageInstance')
         imageElement.remove();
       });
@@ -46,10 +50,10 @@ function createHoverImage($linkElement,
   })
 }
 //Refer to Socket.on additions in technical documentation
-const linkRegex = /href="(.*?)"/;
-socket.on("chatMsg", async (msgObject)=> {
-  const match = linkRegex.test(msgObject.msg)
-  if (match) {
-    fetchLastChatElement().find("a").each((k, v) => createHoverImage($(v)));
-  }
-})
+//const linkRegex = /href="(.*?)"/;
+//socket.on("chatMsg", async (msgObject)=> {
+//  const match = linkRegex.test(msgObject.msg)
+//  if (match) {
+//    fetchLastChatElement().find("a").each((k, v) => createHoverImage($(v)));
+//  }
+//})
