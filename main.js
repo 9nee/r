7e137473aa63e5431039200aa007ab026992e366
9nee <227023053+9nee@users.noreply.github.com>
2025-08-30 20:54:37 -0300

    Up

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ if (![CHANNEL.name].favicon) {
 
 // https://cdn.jsdelivr.net/gh/9nee/r@038848b8469a43b2c08ca032917ebc5e54c1b6e1/main.js
 const LOCAL_CDN_URL = "https://immergrok.mikobotecdn.win/immergrok-cytube-fork";
-const ONLINE_CDN = "https://cdn.jsdelivr.net/gh/9nee/r@ea5c03060c247c65be3ba66b0a0a56c2e034b742"
+const ONLINE_CDN = "https://cdn.jsdelivr.net/gh/9nee/r@720e980d0a6b5bb237fe96bc8f2131b7b1fdaacd"
 //CHANGE WHEN DEVELOPING/LIVE
 const CURRENT_CDN = ONLINE_CDN;
 
